3d1c03ef07b37fb53c139d3d90a0154ec4a23daa
lfzdavid <lemaagicien@gmail.com>
2021-08-19 22:07:45 +0200

    add test access createform

--- tests/AppBundle/Controller/TaskControllerTest.php
+++ tests/AppBundle/Controller/TaskControllerTest.php
@@ -34,16 +34,30 @@ class TaskControllerTest extends DefaultControllerTest
         
     }
 
-    public function testGuestCantAccessList(): void
+    public function testGuestCantAccessListOrCreate():void
     {
-        $this->guestClient->request('GET', '/tasks');
-        $response = $this->guestClient->getResponse();
-        /** Authenticate user is redirected to login page */
-        $this->assertEquals(302, $response->getStatusCode());
-        $this->assertContains(
-            'Redirecting to http://localhost/login',
-            $response->getContent()
-        );
+        $routes = ['/tasks', '/tasks/create'];
+        foreach ($routes as $route) {
+            $this->guestClient->request('GET', '/tasks');
+            $response = $this->guestClient->getResponse();
+            /** Authenticate user is redirected to login page */
+            $this->assertEquals(302, $response->getStatusCode());
+            $this->assertContains(
+                'Redirecting to http://localhost/login',
+                $response->getContent()
+            );
+        }
     }
-    
+
+    public function testGetCreateForm():void
+    {
+        $crawler = $this->authClient->request('GET','/tasks/create');
+        $response = $this->authClient->getResponse();
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals(1, $crawler->filter('input#task_title')->count());
+        $this->assertEquals(1, $crawler->filter('textarea#task_content')->count());
+        $this->assertEquals(1, $crawler->selectButton('Ajouter')->count());
+
+    }
+
 }
